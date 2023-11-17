a399905e32271d7e886b3dd36e143d352cae2977
sandra7968 <sandrapylee@gmail.com>
2023-11-18 00:54:40 +0530

    quiz-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { quizData } from './components/quizData';
+
+const backgroundImages =  [
+  'https://wallpapers.com/images/hd/giant-stars-4k-space-q03wbnsie2ul47hh.jpg',
+  'https://a-static.besthdwallpaper.com/planets-wallpaper-3840x2160-197_54.jpg',
+  'https://images.hdqwalls.com/wallpapers/blue-ruby-nebula-4k-no.jpg',
+  'https://i.pinimg.com/originals/ec/07/3d/ec073d1de243d025ca8699ab5dddc78b.jpg',
+  'https://wallpapercave.com/wp/wp3508237.jpg',
+  'https://wallpapercave.com/wp/wp8093250.jpg',
+  'https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77701888978.jpg',
+  'https://cdn.wallpapersafari.com/56/32/Or1CAT.jpg',
+  'https://wallpaperaccess.com/full/384578.jpg',
+  'https://static.vecteezy.com/system/resources/previews/024/448/956/large_2x/space-wallpaper-banner-background-stunning-view-of-a-cosmic-galaxy-with-planets-and-space-objects-elements-of-this-image-furnished-by-nasa-generate-ai-free-photo.jpg'
+
+ 
+ ];
 
 function App() {
+  const [ currentQ, setCurrentQ] = useState(0)
+  const [showScore, setShowScore] = useState(false)
+  const [score,setScore] = useState(0)
+  const [bg,setBg] = useState('https://images2.alphacoders.com/132/1329016.png')
+ const getRandomBg = ()=>{
+  const randomBg = backgroundImages[Math.floor(Math.random()*backgroundImages.length)]
+  setBg(randomBg)
+ }
+ const handleAnswerButton = (isCorrect)=>{
+  if(isCorrect === true){
+    setScore(score + 1)
+    
+  }
+  const nextQ = currentQ +1;
+  if(nextQ < quizData.length){
+    setCurrentQ(nextQ)
+  } else{
+    setShowScore(true)
+  }
+}
+
+ useEffect(()=>{
+  getRandomBg()
+ },[handleAnswerButton])
+
+ const handleReset =()=>{
+  setCurrentQ(0)
+  setShowScore(false)
+  setScore(0)
+ }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body' style={{background:`url(${bg})`,backgroundRepeat:'no-repeat',backgroundSize:'cover',transition:'background-image 0.75s ease-out',height:'100vh'}}>
+    <div className='container'>
+      <h1 style={{color:'darkgrey', fontSize:'40px'}}>The Ultimate Space Quiz</h1>
+         {showScore?
+          (
+          <div className='score-card'>
+            Your Score : <br /> <span style={{fontSize:'90px'}}>{score}/10</span>
+            <div className="replay-btn">
+              <button onClick={handleReset}>Try Again</button>
+            </div>
+          </div>
+           ):
+           (
+          <div className='question-card'>
+            <h3 className='qNum'>{currentQ+1}/{quizData.length}</h3>
+            <h3 className='qText'>{quizData[currentQ].questionText}</h3>
+            {quizData[currentQ].answerOption.map((answerOption)=>(
+               <div className='btn'>
+              <button onClick={()=>handleAnswerButton(answerOption.isCorrect)}>{answerOption.answerText}</button>
+              </div>
+            ))}
+          </div>
+           )
+            }
+    </div>
     </div>
   );
 }
---
+++ src/components/quizData.js
@@ -0,0 +1,92 @@
+export const quizData = [
+    {
+        questionText:'Which is the smallest planet within our solar system?',
+        answerOption: [
+            {answerText:'Earth',isCorrect: false},
+            {answerText:'Mercury',isCorrect: true},
+            {answerText:'Jupiter',isCorrect: false},
+            {answerText:'Venus',isCorrect: false}
+         ]
+    },
+    {
+        questionText:'The moon called Titan orbits which planet?',
+        answerOption: [
+            {answerText:'Jupiter',isCorrect: false},
+            {answerText:'Neptune',isCorrect: false},
+            {answerText:'Mars',isCorrect: false},
+            {answerText:'Saturn',isCorrect: true}
+         ]
+    },
+    {
+        questionText:'Which is the brightest planet in the night sky?',
+        answerOption: [
+            {answerText:'Venus',isCorrect: true},
+            {answerText:'Saturn',isCorrect: false},
+            {answerText:'Mercury',isCorrect: false},
+            {answerText:'Mars',isCorrect: false}
+         ]
+    },
+    {
+        questionText:'Which planet has the most moons?',
+        answerOption: [
+            {answerText:'Jupiter',isCorrect: false},
+            {answerText:'Saturn',isCorrect: true},
+            {answerText:'Earth',isCorrect: false},
+            {answerText:'Venus',isCorrect: false}
+         ]
+    },
+    {
+        questionText:'Which planet has the fastest rotation?',
+        answerOption: [
+            {answerText:'Earth',isCorrect: false},
+            {answerText:'Mars',isCorrect: false},
+            {answerText:'Jupiter',isCorrect: true},
+            {answerText:'Saturn',isCorrect: false}
+         ]
+    },
+    {
+        questionText:'Which planet has the most volcanoes?',
+        answerOption: [
+            {answerText:'Venus',isCorrect: true},
+            {answerText:'Saturn',isCorrect: false},
+            {answerText:'Mars',isCorrect: false},
+            {answerText:'Earth',isCorrect: false}
+         ]
+    },
+    {
+        questionText:'Which planet spins backwards relative to the others?',
+        answerOption: [
+            {answerText:'Neptune',isCorrect: false},
+            {answerText:'Mercury',isCorrect: false},
+            {answerText:'Venus',isCorrect: true},
+            {answerText:'Jupiter',isCorrect: false}
+         ]
+    },
+    {
+        questionText:'Which planet has supersonic winds?',
+        answerOption: [
+            {answerText:'Neptune',isCorrect: true},
+            {answerText:'Mars',isCorrect: false},
+            {answerText:'Saturn',isCorrect: false},
+            {answerText:'Mercury',isCorrect: false}
+         ]
+    },
+    {
+        questionText:'How long is one year on Jupiter?',
+        answerOption: [
+            {answerText:'3 Earth years',isCorrect: false},
+            {answerText:'12 Earth years',isCorrect: true},
+            {answerText:'5 Earth years',isCorrect: false},
+            {answerText:'10 Earth years',isCorrect: false}
+         ]
+    },
+    {
+        questionText:'How many moons does Earth have?',
+        answerOption: [
+            {answerText:'Three',isCorrect: false},
+            {answerText:'Six',isCorrect: false},
+            {answerText:'Two',isCorrect: false},
+            {answerText:'One',isCorrect: true}
+         ]
+    }
+]
